4fdc340c3766c4efc79f14c46718c67cd0d4eeb7
Neal Chris <tmbarnhart1@gmail.com>
2023-08-10 21:28:00 -0400

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Password Generator Starter Code
+# Password Generator - Javascript
+
+Password Generator powered by JavaScript
+
+# User Story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+# Acceptance Criteria
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+# Mock-up Image of Site
+
+Image Coming Soon
+
+# Link to Live Site
+
+https://thomas-barnhart.github.io/Password-Generator-JavaScript/
+
+# Link to GitHub
+
+https://github.com/Thomas-Barnhart/Password-Generator-JavaScript
+
+# Final Use
+
+This site can be used to generate a password ranging from 8 to 128 characters in lenght. Using a either lowercase, uppercase, numeric and/or special characters. User will be alerted if they go outside of the defined parameters.
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,85 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var Criteria = {
+  Length: 0,
+  LowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  UpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  Number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  Character: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", ",", ".", "/", ":", ";", "<", ">", "=", "?", "[", "]", "_", "`", "{", "}", "|", "\'", "~"]
+}
+
+function generatePassword() {
+  var text = "";
+  var passwordLength = 0;
+  var upperCase;
+  var lowerCase;
+  var numbers;
+  var specialChar;
+  passwordLength = 0;
+  Criteria.Length = 0;
+  text = "";
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters.");
+    if (passwordLength === null) {
+      return "Your secure password";
+    }
+    else {
+      if (!isFinite(passwordLength)) {
+        alert("You did not enter a number");
+        return "Your secure password";
+      }
+      else {
+        if (passwordLength < 8 || passwordLength > 128) {
+          alert("Password must be between 8 and 128 characters.");
+          return "Your secure password";
+        }
+        else {
+          showPrompts();
+          while (Criteria.Length < passwordLength) {
+            if (lowerCase === false && upperCase === false && numbers === false && specialChar === false) {
+              alert("You must select at least one criteria of lowercase, uppercase, numbers or special characters")
+              showPrompts();
+            }
+            else {
+              if (lowerCase === true && Criteria.Length < passwordLength) {
+                var lc = Criteria.LowerCase[Math.floor(Math.random() * 26)]
+                text = text + lc;
+                Criteria.Length++;
+              }
+              if (specialChar === true && Criteria.Length < passwordLength) {
+                var sc = Criteria.Character[Math.floor(Math.random() * 30)]
+                text = text + sc;
+                Criteria.Length++;
+              }
+              if (upperCase === true && Criteria.Length < passwordLength) {
+                var uc = Criteria.UpperCase[Math.floor(Math.random() * 26)]
+                text = text + uc;
+                Criteria.Length++;
+              }
+              if (numbers === true && Criteria.Length < passwordLength) {
+                var num = Criteria.Number[Math.floor(Math.random() * 10)]
+                text = text + num;
+                Criteria.Length++;
+              }
+            }
+          }
+        }
+      }
+    }
+    return text;
+    function showPrompts() {
+      lowerCase = confirm("Do you want to use lower case letters?");
+      upperCase = confirm("Do you want to use upper case letters?");
+      numbers = confirm("Do you want to use numbers?");
+      specialChar = confirm("Do you want to use any special characters?");
+    }
+  }
+}
